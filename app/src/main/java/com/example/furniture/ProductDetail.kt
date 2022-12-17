8e81d0e55d06a21280ecf33c5cf98b3243d1ab8b
alyam <dalyamanis4@gmail.com>
2022-12-17 23:44:50 +0300

    add fargments

--- app/src/main/java/com/example/furniture/ProductDetail.kt
+++ app/src/main/java/com/example/furniture/ProductDetail.kt
@@ -1,35 +1,49 @@
 
     package com.example.furniture
 
+    import android.annotation.SuppressLint
+    import android.content.ContentValues.TAG
     import android.content.Intent
     import android.os.Bundle
+    import android.util.Log
     import android.view.*
     import android.widget.ImageView
     import android.widget.TextView
     import androidx.fragment.app.Fragment
     import com.example.furniture.databinding.FragmentProductDetailBinding
-    import kotlin.properties.Delegates
 
     class ProductDetail : Fragment() {
 
         private var _binding: FragmentProductDetailBinding? = null
         private val binding get() = _binding!!
-        private lateinit var nameId: String
-        private lateinit var priceId: String
-        private var imageId by Delegates.notNull<Int>()
-        private lateinit var descriptionId: String
+        companion object {
+            var name = "name"
+            var furnatureName = "Name"
+            var descrption = "desc"
+            var furnatureDescrption = "Description"
+            var image = "image"
+            var imagepath = 0
+            var price = "price"
+            var furnaturePrice = "price"
+
+
+        }
 
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             setHasOptionsMenu(true)
-            arguments?.let {
+          /*  arguments?.let {
                 nameId = it.getString("name").toString()
                 priceId = it.getString("price").toString()
                 imageId = it.getInt("image")
                 descriptionId = it.getString("description").toString()
+*/
+            setHasOptionsMenu(true)
+
+            Log.d(TAG, "onCreate Called")
             }
 
-        }
+
 
 
         override fun onCreateView(
@@ -37,31 +51,44 @@
             savedInstanceState: Bundle?
         ): View? {
             // Inflate the layout for this fragment
+            Log.d(TAG, "onCreateView Called")
+
             return inflater.inflate(R.layout.fragment_product_detail, container, false)
         }
 
+        @SuppressLint("ResourceType")
         override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            arguments?.let {
+                furnatureName = it.getString(name).toString()
+                furnatureDescrption = it.getString(descrption).toString()
+                imagepath = it.getInt(image)
+                furnaturePrice= it.getString(price).toString()
 
-            var name: TextView = binding.tvNameDetaile
-            name.text = nameId
 
+            }
+            var furnature_name: TextView = view.findViewById(R.id.tv_nameDetaile)
+            var furnature_Price: TextView = view.findViewById(R.id.tv_priceDetailed)
+            var coffee_Description: TextView = view.findViewById(R.id.detaile)
+            var furnatur_image: ImageView = view.findViewById(R.id.imageViewDetailed)
 
-            var price: TextView = binding.tvPriceDetailed
-            price.text = priceId
+            furnature_name.text = furnatureName
+            coffee_Description.text = furnatureDescrption
+            furnatur_image.setImageResource(imagepath)
+            furnature_Price.text = furnaturePrice
+            Log.d(TAG, "onViewCreatedD Called")
 
-            var description:TextView = binding.detaile
-            description.text = descriptionId
-
-            var image: ImageView = binding.imageViewDetailed
-
-            image.setImageResource(imageId)
 
         }
         override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
             inflater.inflate(R.menu.main_menu, menu)
+            Log.d(TAG, "onCreateOptionsMenuD Called")
+
         }
 
         override fun onOptionsItemSelected(item: MenuItem): Boolean {
+            Log.d(TAG, "onOptionsItemSelectedD Called")
+
             return when (item.itemId) {
                 R.id.share -> {
                     val sendIntent: Intent = Intent().apply {
